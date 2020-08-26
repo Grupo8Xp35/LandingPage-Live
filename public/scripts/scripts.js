dcd5b2b54cb4417d25b5024059d869e2b8a34baa
Alvaro Coelho Jesus <alvim.coelho.jesus@gmail.com>
2020-08-25 22:04:21 -0300

    Alterando hora live

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,6 +1,6 @@
 import Countdown from "./contador.js";
 
-const dataLive = new Countdown("27 August 2020 19:00:00 GMT-0300");
+const dataLive = new Countdown("27 August 2020 20:00:00 GMT-0300");
 const tempos = document.querySelectorAll("[data-time]");
 
 function contRegres(){
